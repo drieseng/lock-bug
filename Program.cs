4ca0a0a83f4e0f654f4158e4f2404d995aff931c
Gert Driesen <gert.driesen@infrabel.be>
2025-02-27 10:23:34 +0100

    Initial version.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static readonly Lock SyncLock = new Lock();
+
+        static void Main(string[] args)
+        {
+            lock (SyncLock)
+            {
+
+            }
+        }
+    }
+}
